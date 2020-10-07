9121291bf7612b0747ca0d0152165d7f8565be42
ISlack7 <ian.slack@patelhs.org>
2020-10-07 13:04:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.scanner;
+public class  {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner("System.in");
+
+    System.out.print("Hi, what's your name?");
+    String name = scan.next();
+  }
+}
